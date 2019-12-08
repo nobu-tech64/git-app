cf885e2bf1911edcedc07db9441a987b8c5fd07b
nobu-tech64 <douji533rikou@icloud.com>
2019-12-08 15:44:25 +0900

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,12 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
-  # GET /articles
-  # GET /articles.json
+
   def index
     @articles = Article.all
   end
 
-  # GET /articles/1
-  # GET /articles/1.json
+
   def show
   end
 
@@ -17,12 +15,11 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-  # GET /articles/1/edit
+
   def edit
   end
 
-  # POST /articles
-  # POST /articles.json
+
   def create
     @article = Article.new(article_params)
 
@@ -37,8 +34,7 @@ class ArticlesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /articles/1
-  # PATCH/PUT /articles/1.json
+
   def update
     respond_to do |format|
       if @article.update(article_params)
@@ -51,8 +47,7 @@ class ArticlesController < ApplicationController
     end
   end
 
-  # DELETE /articles/1
-  # DELETE /articles/1.json
+
   def destroy
     @article.destroy
     respond_to do |format|
